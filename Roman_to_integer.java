b0bf5203d7fb4e683d30e688ab6120e7abbca38a
Kuldeep Chauhan <124709046+ku05@users.noreply.github.com>
2023-10-04 07:02:26 +0530

    Create Roman_to_integer.java

---
+++ Roman_to_integer.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String roman = br.readLine().trim();
+            Solution ob = new Solution();
+            System.out.println(ob.romanToDecimal(roman));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Finds decimal value of a given roman numeral
+    public int romanToDecimal(String s) {
+        Map<Character, Integer> romanToInteger = new HashMap<>();
+        romanToInteger.put('I', 1);
+        romanToInteger.put('V', 5);
+        romanToInteger.put('X', 10);
+        romanToInteger.put('L', 50);
+        romanToInteger.put('C', 100);
+        romanToInteger.put('D', 500);
+        romanToInteger.put('M', 1000);
+        
+        int result = 0;
+        int prevValue = 0;
+        
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int currValue = romanToInteger.get(s.charAt(i));
+            
+            if (currValue < prevValue) {
+                result -= currValue;
+            } else {
+                result += currValue;
+            }
+            
+            prevValue = currValue;
+        }
+        
+        return result;
+    }
+}
